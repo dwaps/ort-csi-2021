9b52ccd3866604bf328993ed17c4f4b57ad2f1f3
Squid-Hack <n.bosc1998@orange.fr>
2021-05-06 15:59:03 +0200

    [ADD] event

---
+++ index.js
@@ -0,0 +1,8 @@
+// Importer la classe EventEmitter
+const EventEmitter = require('./event-emitter');
+const emitter = new EventEmitter();
+
+
+// emitter.on('MY_EVENT', () => console.log('event emitted !'));
+// // Pas besoin de la méthode : emitter.once()
+emitter.emit('EVENT');
