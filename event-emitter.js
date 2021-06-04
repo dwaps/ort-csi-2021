ae1cdc4fc064df95a0f1eb0be2cb96dac5cc1fc5
Kenny <kantoine-edouard@consultum.fr>
2021-06-05 01:43:56 +0200

    TP ANGULAR TODOLIST :)

--- event-emitter.js
+++
@@ -1,7 +0,0 @@
-class EventEmitter {
-  emit() {
-    return "Chargement terminé";
-  }
-}
-
-module.exports = EventEmitter;
--- index.js
+++
@@ -1,8 +0,0 @@
-const EventEmitter = require("./event-emitter");
-const loader = require("./loader");
-
-const emitter = new EventEmitter();
-const ld = new loader();
-
-ld.on(); // Lancement du chargment
-emitter.emit(); //fin du chargement
