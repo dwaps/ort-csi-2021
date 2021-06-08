3d2b6822ea9228ab5eba1c8e57eadd82df03e7ff
Michael CORNILLON <contact@dwaps.fr>
2021-06-08 15:22:37 +0200

    ex: routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,37 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
-
+import { Route, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
-import { MyDirectiveDirective } from './my-directive.directive';
+import { BooksComponent } from './books/books.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { NavComponent } from './nav/nav.component';
+
+
+const routes: Array<Route> = [
+  { path: 'home', component: HomeComponent },
+  { path: 'books', component: BooksComponent },
+  { path: 'contact', component: ContactComponent },
+];
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    MyDirectiveDirective,
+    FooterComponent,
+    HeaderComponent,
+    NavComponent,
+    ContactComponent,
+    HomeComponent,
+    BooksComponent,
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
