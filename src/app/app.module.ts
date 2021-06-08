c1dd6631b58c6541fb3dde90355247e43fe55bdb
Michael CORNILLON <contact@dwaps.fr>
2021-06-08 09:58:56 +0200

    communication inter composants

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { ChildComponent } from './child/child.component';
+import { ParentComponent } from './parent/parent.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ChildComponent,
+    ParentComponent,
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
